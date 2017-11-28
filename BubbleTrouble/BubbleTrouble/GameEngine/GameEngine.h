010d62657a9146fa61c5a6ff0c54e46a77291140
Sighvatur Gíslason <hvatig@gmail.com>
2017-11-28 21:51:43 +0100

    Made TextureManager and tested

--- BubbleTrouble/BubbleTrouble/GameEngine/GameEngine.h
+++ BubbleTrouble/BubbleTrouble/GameEngine/GameEngine.h
@@ -7,28 +7,54 @@
 #include <vector>
 #include "headers\Menu.h"
 #include "headers\Map.h"
+#include "SDL_image.h"
 
 class GameEngine {
 private:
 	GameObject * player;
 	std::vector<GameObject*> bubbles;
 	Map * map;
-public:
-	static SDL_Renderer * Renderer;
+	SDL_Window * window;
 
-	GameEngine(){
+
+public:
+	SDL_Renderer * renderer;
+	SDL_Rect srcrect, destrect;
+	GameEngine(std::string title, int winposx, int winposy, int winwidth, int winheight, SDL_WindowFlags flag){
+		SDL_Init(SDL_INIT_EVERYTHING);
+		window = SDL_CreateWindow(title.c_str(), winposx, winposy, winwidth, winheight, flag);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 		std::cout << "Game Engine Constructed.\n";
 	};
+
 	~GameEngine(){
 		delete player;
 		bubbles.clear();
+		
 	};
 
 	void init() {
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_ShowWindow(window);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
 		player = new GameObject();
 		player->addComponent<TileHandler>();
-		player->addComponent<Vector2D<float>>(0.1f, 0.2f);
-		std::cout << player->getComponent<Vector2D<float>>()->x <<  "  " <<player->getComponent<Vector2D<float>>()->y << std::endl;
+		player->addComponent<TextureHandler>(renderer);
+		player->getComponent<TextureHandler>()->loadTexture("C:/Users/hvati/source/repos/AdvProg_Bubble/BubbleTrouble/BubbleTrouble/assets/enemy.png");
+		player->getComponent<TextureHandler>()->loadTexture("assets/weirdguy.png");
+
+		srcrect.h = 128;
+		srcrect.w = 128;
+		srcrect.x = 0;
+		srcrect.y = 0;
+		destrect = srcrect;
+	}
+	void draw() {
+		SDL_RenderClear(renderer);
+
+		SDL_RenderCopyEx(renderer, player->getComponent<TextureHandler>()->getTexture(), &srcrect, &destrect, NULL, NULL, SDL_FLIP_NONE);
+		SDL_RenderPresent(renderer);
 	}
 
 	/// Deletes invalidated game objects
--- BubbleTrouble/BubbleTrouble/main.cpp
+++ BubbleTrouble/BubbleTrouble/main.cpp
@@ -1,39 +1,36 @@
 #include "SDL.h"
+#include "SDL_image.h"
 #include <iostream>
 #include "GameEngine\GameEngine.h"
 
 
 int main(int /*argc*/, char ** /*argv*/) {
-	GameEngine * Game = new GameEngine();
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window * window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |	SDL_RENDERER_PRESENTVSYNC);
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	Game->init();
 
+	GameEngine * Game = new GameEngine("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 480, SDL_WINDOW_HIDDEN);
+
+	Game->init();
 	bool quit = false;
 
 	//Event handler
-	SDL_Event e_xOut;
+	SDL_Event e;
 
 	//While application is running
 	while (!quit)
 	{
 		//Handle events on queue
-		while (SDL_PollEvent(&e_xOut) != 0)
+		while (SDL_PollEvent(&e) != 0)
 		{
 			//User requests quit
-			if (e_xOut.type == SDL_QUIT)
+			if (e.type == SDL_QUIT)
 			{
 				quit = true;
 			}
 		}
 
+		Game->draw();
+
 	}
 	///
 	//SDL_Delay(30000);
-
 	return 0;
 }
