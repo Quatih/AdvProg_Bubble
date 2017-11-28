e34d44904296cace6b72b54b4f7ee478f36ae977
Sighvatur Gíslason <hvatig@gmail.com>
2017-11-28 18:59:38 +0100

    Moved renderer and window to GameEngine.

--- BubbleTrouble/BubbleTrouble/main.cpp
+++ BubbleTrouble/BubbleTrouble/main.cpp
@@ -6,13 +6,11 @@
 
 
 int main(int /*argc*/, char ** /*argv*/) {
-	GameEngine * Game = new GameEngine();
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window * window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 480, SDL_WINDOW_SHOWN);
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |	SDL_RENDERER_PRESENTVSYNC);
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
+
+	GameEngine * Game = new GameEngine("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 480, SDL_WINDOW_SHOWN);
+	SDL_SetRenderDrawColor(Game->renderer, 255, 255, 255, 255);
+	SDL_RenderClear(Game->renderer);
+	SDL_RenderPresent(Game->renderer);
 	Game->init();
 	SDL_Delay(10000);
 
