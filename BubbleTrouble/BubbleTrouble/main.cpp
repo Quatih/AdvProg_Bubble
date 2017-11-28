42fec824689d44f8d47eeb95fb288f6bf1673773
Navin <n.r.reddy@student.utwente.nl>
2017-11-28 19:59:49 +0100

    changed object name e to e_xOut

--- BubbleTrouble/BubbleTrouble/main.cpp
+++ BubbleTrouble/BubbleTrouble/main.cpp
@@ -16,16 +16,16 @@ int main(int /*argc*/, char ** /*argv*/) {
 	bool quit = false;
 
 	//Event handler
-	SDL_Event e;
+	SDL_Event e_xOut;
 
 	//While application is running
 	while (!quit)
 	{
 		//Handle events on queue
-		while (SDL_PollEvent(&e) != 0)
+		while (SDL_PollEvent(&e_xOut) != 0)
 		{
 			//User requests quit
-			if (e.type == SDL_QUIT)
+			if (e_xOut.type == SDL_QUIT)
 			{
 				quit = true;
 			}
